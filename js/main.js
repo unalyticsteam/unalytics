5beba02d83c48025436febbf9d50b6c12e714ddc
hebermudezg <hebermudezg@unal.edu.co>
2019-01-16 01:28:23 -0500

    cambio en la seccion temas html, se cambio por panel deseglable

--- js/main.js
+++ js/main.js
@@ -79,5 +79,20 @@ function main() {
 }());
 
 
+/* Funcion para la lista desplegable de los temas */ 
+	
+$(document).ready(function() {
+    $('[id^=detail-]').hide();
+    $('.toggle').click(function() {
+        $input = $( this );
+        $target = $('#'+$input.attr('data-toggle'));
+        $target.slideToggle();
+    });
+});	
+	
+	
+	
+	
+
 }
 main();
