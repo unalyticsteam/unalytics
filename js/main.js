f8d2a315f53f71abe82fe04a26b871f57042c21a
hebermudezg <hebermudezg@unal.edu.co>
2019-04-17 10:29:45 -0500

    cambios index modificacion all3

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ function main() {
 }());
 
 
-/* Funcion para la lista desplegable de los temas */ 
+/****************************Funcion para la lista desplegable de los temas ***************************/ 
 	
 $(document).ready(function() {
     $('[id^=detail-]').hide();
@@ -90,7 +90,39 @@ $(document).ready(function() {
     });
 });	
 	
+	
+	
+/************************** Funcion para la seccion de noticias *********************/
+	
+$(document).ready(function(){
+    
+	var clickEvent = false;
+	$('#myCarousel').carousel({
+		interval:   4000	
+	}).on('click', '.list-group li', function() {
+			clickEvent = true;
+			$('.list-group li').removeClass('active');
+			$(this).addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.list-group').children().length -1;
+			var current = $('.list-group li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.list-group li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+})
 
+$(window).load(function() {
+    var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+    var itemlength = $('#myCarousel .item').length;
+    var triggerheight = Math.round(boxheight/itemlength+1);
+	$('#myCarousel .list-group-item').outerHeight(triggerheight);
+});
 	
 	
 	
